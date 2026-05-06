27903163a9ffd0b742081f649a0d55d0cf6edfc1
fluctuatin <tanernyong@gmail.com>
2026-05-06 14:12:09 +0800

    Burger Always Open Temp
    Leaving the Burger menu always open for now till I can find a way to fix it.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //Attempting to get the burger menu to respond to a click and change the display mode on the navbar
 window.onload = init;
+
 function init(){
+    
     const nav = document.getElementById("list");
-    document.getElementById("burger").addEventListener('click', () => {
-        nav.classList.toggle('show');
+    const burg = document.getElementById("burger");
+    
+    burg.addEventListener('click', () => {
+        nav.style.display = "flexbox";
         console.log("Working");
     });
 }
